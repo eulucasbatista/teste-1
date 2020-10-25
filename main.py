6dad0d244984610e6cd338c0b0065cd1df38b40f
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2020-10-25 20:32:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+nota1 = 2
+nota2 = 3
+nota3 = 8
+
+media = (nota1 + nota2 + nota3) / 3
+
+if(media >= 7):
+    print("Aprovado")
+elif (media >= 5) and (media < 7):
+    print("Reprovado")
+else:
+    print("Reprovado")
+print("Boa sorte na proxima!")
